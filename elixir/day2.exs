d9dd960e09f58b347f8647d35c731100f9055a66
Fillipe Rosini <filliperosini@gmail.com>
2018-12-03 17:04:16 -0200

    Added day 2 part 2 Elixir resolution

--- elixir/day2.exs
+++ elixir/day2.exs
@@ -25,7 +25,46 @@ defmodule Day2 do
     end
 
     defp multiply_tuple({x, y}), do: x * y
+
+   def part2(path) do
+        list = File.read!(path)
+            |> String.split("\r\n")
+            |> Stream.filter(fn s -> s != "" end)
+
+        list
+        |> Stream.with_index()
+        |> Stream.map(fn {x, i} -> get_diffs(list, {x, i}) end)
+        |> Enum.find(fn d -> Enum.count(d) > 0 end)
+        |> parse_result()
+        |> IO.inspect()
+    end
+
+    defp get_diffs(list, {x, i}) do
+        list
+        |> Stream.drop(i + 1)
+        |> Stream.map(fn y -> get_diff(x, y) end)
+        |> Stream.filter(&diff_valid?/1)
+    end
+
+    defp get_diff(s1, s2) do
+        String.myers_difference(s1, s2)
+        |> Enum.group_by(fn {k, _} -> k end, fn {_, v} -> v end)
+    end
+
+    defp diff_valid?(%{:del => [del], :ins => [ins]}) do
+        String.length(del) == 1 && String.length(ins) == 1
+    end
+
+    defp diff_valid?(_) do
+        false
+    end
+
+    defp parse_result(xs) do
+        [x | _] = Enum.take(xs, 1)
+        x.eq |> Enum.join()
+    end
 end
 
 path = "../inputs/day2.txt"
 Day2.part1(path)
+Day2.part2(path)

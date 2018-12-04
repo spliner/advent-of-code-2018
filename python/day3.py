7dbe3e4e6c9ed911eb5236b2499b6c670ef24b19
Fillipe Rosini <filliperosini@gmail.com>
2018-12-03 22:30:10 -0200

    Added x2 and y2 to Claim

--- python/day3.py
+++ python/day3.py
@@ -4,7 +4,7 @@ import re
 INPUT = '../inputs/day3.txt'
 REGEX = r'#(?P<id>\d+)\s@\s(?P<x>\d+),(?P<y>\d+):\s(?P<w>\d+)x(?P<h>\d+)'
 
-Claim = collections.namedtuple('Claim', 'id x y width height')
+Claim = collections.namedtuple('Claim', 'id x1 y1 x2 y2 width height')
 
 
 def part1(claims):
@@ -19,14 +19,21 @@ def parsefile(path, regex):
 def parseline(line, regex):
     match = re.match(regex, line)
     groups = match.groupdict()
-    return Claim(int(groups['id']), int(groups['x']), int(groups['y']), int(groups['w']), int(groups['h']))
+    x1 = int(groups['x'])
+    y1 = int(groups['y'])
+    w = int(groups['w'])
+    h = int(groups['h'])
+    x2 = x1 + w
+    y2 = y1 + h
+    return Claim(
+        int(groups['id']), x1, y1, x2, y2, w, h)
 
 
 if __name__ == '__main__':
     claims = parsefile(INPUT, REGEX)
     part1_test_data = [
-        Claim(1, 1, 3, 4, 4),
-        Claim(2, 3, 1, 4, 4),
-        Claim(3, 5, 5, 2, 2)
+        Claim(1, 1, 3, 5, 8, 4, 4),
+        Claim(2, 3, 1, 8, 5, 4, 4),
+        Claim(3, 5, 5, 7, 7, 2, 2)
     ]
     part1(claims)

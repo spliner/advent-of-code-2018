9c2d414e52ebf0cb29565d20edfe3a6597f42bec
Fillipe Rosini <filliperosini@gmail.com>
2018-12-03 22:23:57 -0200

    Improved file parsing

--- python/day3.py
+++ python/day3.py
@@ -1,7 +1,10 @@
+import collections
 import re
 
 INPUT = '../inputs/day3.txt'
 REGEX = r'#(?P<id>\d+)\s@\s(?P<x>\d+),(?P<y>\d+):\s(?P<w>\d+)x(?P<h>\d+)'
+
+Claim = collections.namedtuple('Claim', 'id x y width height')
 
 
 def part1(claims):
@@ -16,14 +19,14 @@ def parsefile(path, regex):
 def parseline(line, regex):
     match = re.match(regex, line)
     groups = match.groupdict()
-    groupid = int(groups['id'])
-    x = int(groups['x'])
-    y = int(groups['y'])
-    width = int(groups['w'])
-    height = int(groups['h'])
-    return (groupid, x, y, width, height)
+    return Claim(int(groups['id']), int(groups['x']), int(groups['y']), int(groups['w']), int(groups['h']))
 
 
 if __name__ == '__main__':
     claims = parsefile(INPUT, REGEX)
+    part1_test_data = [
+        Claim(1, 1, 3, 4, 4),
+        Claim(2, 3, 1, 4, 4),
+        Claim(3, 5, 5, 2, 2)
+    ]
     part1(claims)

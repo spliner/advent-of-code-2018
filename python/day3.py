42944c09d97da0a08caae14763ffa8cc2d498893
Fillipe Rosini <filliperosini@gmail.com>
2018-12-04 20:05:18 -0200

    Added day 3 part 1 solution

--- python/day3.py
+++ python/day3.py
@@ -10,24 +10,40 @@ Claim = collections.namedtuple('Claim', 'id bounds width height')
 
 
 def part1(claims):
+    intersections_points = set()
     for index, claim in enumerate(claims):
         for comparison in claims[index + 1:]:
             intersection = calculate_intersection(
                 claim.bounds, comparison.bounds)
-            isvalid = isvalid_rectangle(intersection)
+            if not intersection:
+                continue
+            points = get_all_points(intersection)
+            intersections_points.update(points)
+    return len(intersections_points)
 
 
 def calculate_intersection(rectangle1, rectangle2):
     x1 = max(rectangle1.p1.x, rectangle2.p1.x)
     y1 = max(rectangle1.p1.y, rectangle2.p1.y)
-    x2 = max(rectangle1.p2.x, rectangle2.p2.x)
-    y2 = max(rectangle1.p2.y, rectangle2.p2.y)
-    return Rectangle(Point(x1, y1), Point(x2, y2))
+    x2 = min(rectangle1.p2.x, rectangle2.p2.x)
+    y2 = min(rectangle1.p2.y, rectangle2.p2.y)
+    intersection = Rectangle(Point(x1, y1), Point(x2, y2))
+    isvalid = intersection.p1.x <= intersection.p2.x and intersection.p1.y <= intersection.p2.y
+    if not isvalid:
+        return None
+    return intersection
 
 
-def isvalid_rectangle(rectangle):
-    # TODO: Actually check if rectangle is valid
-    return True
+def get_all_points(rectangle):
+    points = []
+    point = rectangle.p1
+    while point.x <= rectangle.p2.x and point.y <= rectangle.p2.y:
+        points.append(point)
+        if point.x < rectangle.p2.x:
+            point = Point(point.x + 1, point.y)
+        else:
+            point = Point(rectangle.p1.x, point.y + 1)
+    return points
 
 
 def parsefile(path, regex):
@@ -42,16 +58,20 @@ def parseline(line, regex):
     y1 = int(groups['y'])
     w = int(groups['w'])
     h = int(groups['h'])
-    x2 = x1 + w
-    y2 = y1 + h
+    x2 = x1 + w - 1
+    y2 = y1 + h - 1
     return Claim(int(groups['id']), Rectangle(Point(x1, y1), Point(x2, y2)), w, h)
 
 
 if __name__ == '__main__':
-    claims = parsefile(INPUT, REGEX)
-    part1_test_data = [
-        Claim(1, Rectangle(Point(1, 3), Point(5, 8)), 4, 4),
-        Claim(2, Rectangle(Point(3, 1), Point(8, 5)), 4, 4),
-        Claim(3, Rectangle(Point(5, 5), Point(7, 7)), 2, 2)
+    lines = [
+        '#1 @ 1,3: 4x4',
+        '#2 @ 3,1: 4x4',
+        '#3 @ 5,5: 2x2'
     ]
-    part1(claims)
+    part1_test_data = [parseline(l, REGEX) for l in lines]
+    assert part1(part1_test_data) == 4
+
+    claims = parsefile(INPUT, REGEX)
+    area = part1(claims)
+    print(area)

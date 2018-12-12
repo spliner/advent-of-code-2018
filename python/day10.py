6e981c3dd590beb621f422e127c96d82efa3b277
Fillipe Rosini <filliperosini@gmail.com>
2018-12-12 13:40:38 -0200

    WIP: Day 10 part 1

--- python/day10.py
+++ python/day10.py
@@ -1,15 +1,13 @@
 import re
-import pygame
 import collections
-import pprint
-import time
 
 from typing import List
+from day10_ui import Day10UIHelper
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 1000
+SCREEN_WIDTH = 500
 BACKGROUND_COLOR = (0, 0, 0)
 POINT_COLOR = (255, 0, 255)
+FONTSIZE = 10
 FONT_COLOR = (255, 255, 255)
 PADDING = 5
 INPUT = '../inputs/day10.txt'
@@ -54,132 +52,55 @@ def get_bounds(points):
     x1 = min(points, key=lambda p: p.position.x).position.x
     y1 = min(points, key=lambda p: p.position.y).position.y
     start = min([x1, y1])
-
     x2 = max(points, key=lambda p: p.position.x).position.x
     y2 = max(points, key=lambda p: p.position.y).position.y
     end = max([x2, y2])
     return (Coordinate(start, start), Coordinate(end, end))
 
 
-def draw_point(point, current_step, screen, bounds, viewport_bounds):
-    w = abs(bounds[0].x) + abs(bounds[1].x)
-    h = abs(bounds[0].y) + abs(bounds[1].y)
+def get_viewport_coordinate(point, current_step, bounds, viewport_scale):
     position = point.get_position(current_step)
-    origin_x = viewport_bounds[1].x / 2
-    origin_y = viewport_bounds[1].y / 2
-    new_x = (position.x + abs(bounds[0].x)) * SCREEN_WIDTH / w
-    new_y = (position.y + abs(bounds[0].y)) * SCREEN_HEIGHT / h
+    new_x = (position.x + bounds[0].x * -1) * viewport_scale
+    new_y = (position.y + bounds[0].y * -1) * viewport_scale
     return Coordinate(round(new_x), round(new_y))
 
 
 if __name__ == '__main__':
-    points = parsefile(INPUT, REGEX)
+    points = parsefile(TEST_INPUT, REGEX)
     bounds = get_bounds(points)
-    viewport_bounds = (Coordinate(0, 0), Coordinate(
-        SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(bounds)
-    print(len(points))
 
-    pygame.init()
-    pygame.font.init()
-    clock = pygame.time.Clock()
-    fontsize = 10
-    font = pygame.font.SysFont('Arial', fontsize)
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    width = abs(bounds[0].x) + abs(bounds[1].x)
+    height = abs(bounds[0].y) + abs(bounds[1].y)
+    aspect_ratio = width / height
+
+    viewport_width = SCREEN_WIDTH
+    viewport_height = round(SCREEN_WIDTH * aspect_ratio)
+    viewport_scale = viewport_width / width
+
+    uiHelper = Day10UIHelper(FONT_COLOR,
+                             FONTSIZE,
+                             PADDING,
+                             viewport_width,
+                             viewport_height)
+
     current_step = 0
     steps = 1
-    done = False
-    keys = {
-        pygame.K_DOWN: False,
-        pygame.K_UP: False,
-        pygame.K_LEFT: False,
-        pygame.K_RIGHT: False
-    }
 
-    foo = [(p, draw_point(p, current_step, screen, bounds, viewport_bounds)) for p in points]
-    bar = sorted(foo, key=lambda f: f[1].x)
-    baz = [(p, p.get_position(1000)) for p in points]
-    pprint.pprint(baz)
-    # for b in baz:
-    #     pygame.draw.rect(screen, POINT_COLOR, (b[0].x, b[0].y, 1, 1))
+    while not uiHelper.done:
+        uiHelper.onloop(BACKGROUND_COLOR, current_step, steps)
 
-    # pprint.pprint(bar)
-    # for i, f in enumerate(foo):
-    #     text = font.render(f'{f[0].position.x}, {f[0].position.y}', False, FONT_COLOR)
-    #     screen.blit(text, (f[1].x, f[1].y))
-    #     pygame.display.flip()
-    #     pygame.time.wait(1000)
-
-    while not done:
-        screen.fill(BACKGROUND_COLOR)
-        text = font.render('Right arrow: next step', False, FONT_COLOR)
-        rightarrow_rect = text.get_rect()
-        rightarrow_rect.left = PADDING
-        rightarrow_rect.top = SCREEN_HEIGHT - rightarrow_rect.height - PADDING
-        screen.blit(text, rightarrow_rect)
-
-        text = font.render('Left arrow: previous step', False, FONT_COLOR)
-        leftarrow_rect = text.get_rect()
-        leftarrow_rect.left = rightarrow_rect.left
-        leftarrow_rect.top = rightarrow_rect.top - leftarrow_rect.height
-        screen.blit(text, leftarrow_rect)
-
-        text = font.render('Up arrow: increase step', False, FONT_COLOR)
-        uparrow_rect = text.get_rect()
-        uparrow_rect.left = SCREEN_WIDTH - uparrow_rect.width - PADDING
-        uparrow_rect.top = SCREEN_HEIGHT - rightarrow_rect.height - PADDING
-        screen.blit(text, uparrow_rect)
-
-        text = font.render('Down arrow: decrease step', False, FONT_COLOR)
-        downarrow_rect = text.get_rect()
-        downarrow_rect.left = SCREEN_WIDTH - downarrow_rect.width - PADDING
-        downarrow_rect.top = uparrow_rect.top - downarrow_rect.height
-        screen.blit(text, downarrow_rect)
-
-        text = font.render(f'Current step: {current_step}', False, FONT_COLOR)
-        screen.blit(text, (PADDING, 0))
-
-        text = font.render(f'Steps: {steps}', False, FONT_COLOR)
-        step_rect = text.get_rect()
-        step_rect.left = SCREEN_WIDTH - step_rect.width - PADDING
-        step_rect.top = PADDING
-        screen.blit(text, step_rect)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    keys[pygame.K_UP] = True
-                elif event.key == pygame.K_DOWN:
-                    keys[pygame.K_DOWN] = True
-                elif event.key == pygame.K_RIGHT:
-                    keys[pygame.K_RIGHT] = True
-                elif event.key == pygame.K_LEFT:
-                    keys[pygame.K_LEFT] = True
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP:
-                    keys[pygame.K_UP] = False
-                elif event.key == pygame.K_DOWN:
-                    keys[pygame.K_DOWN] = False
-                elif event.key == pygame.K_RIGHT:
-                    keys[pygame.K_RIGHT] = False
-                elif event.key == pygame.K_LEFT:
-                    keys[pygame.K_LEFT] = False
-
-        if keys[pygame.K_UP]:
+        if uiHelper.is_up_pressed():
             steps += 1
-        elif keys[pygame.K_DOWN]:
+        elif uiHelper.is_down_pressed():
             steps = max(1, steps - 1)
-        elif keys[pygame.K_RIGHT]:
+        elif uiHelper.is_right_pressed():
             current_step += steps
-        elif keys[pygame.K_LEFT]:
+        elif uiHelper.is_left_pressed():
             current_step = max(0, current_step - steps)
 
-        foo = [(p, draw_point(p, current_step, screen, bounds, viewport_bounds)) for p in points]
-        for f in foo:
-            text = font.render(f'{f[0].velocity.x}, {f[0].velocity.y}', False, FONT_COLOR)
-            screen.blit(text, (f[1].x, f[1].y))
-            # pygame.draw.rect(screen, POINT_COLOR, (f[1].x, f[1].y, 1, 1))
+        viewport_coordinates = [get_viewport_coordinate(
+            p, current_step, bounds, viewport_scale) for p in points]
+        for coordinate in viewport_coordinates:
+            uiHelper.draw_coordinate(coordinate, POINT_COLOR)
 
-        pygame.display.flip()
-        clock.tick(10)
+        uiHelper.draw()
---
+++ python/day10_ui.py
@@ -0,0 +1,108 @@
+import pygame
+
+
+class Day10UIHelper(object):
+    def __init__(self, fontcolor, fontsize, padding, width, height):
+        pygame.init()
+        pygame.font.init()
+
+        self.screen = pygame.display.set_mode((width, height))
+        self.font = pygame.font.SysFont('Arial', fontsize)
+        self.fontcolor = fontcolor
+        self.padding = padding
+        self.width = width
+        self.height = height
+        self.clock = pygame.time.Clock()
+        self.done = False
+        self.keys = {
+            pygame.K_DOWN: False,
+            pygame.K_UP: False,
+            pygame.K_LEFT: False,
+            pygame.K_RIGHT: False
+        }
+
+    def draw_coordinate(self, coordinate, color):
+        pygame.draw.rect(self.screen,
+                         color,
+                         (coordinate.x, coordinate.y, 10, 10))
+
+    def draw(self):
+        pygame.display.flip()
+        self.clock.tick(60)
+
+    def onloop(self, background_color, current_step, steps):
+        self.screen.fill(background_color)
+        self.__render_hud(current_step, steps)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.done = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.keys[pygame.K_UP] = True
+                elif event.key == pygame.K_DOWN:
+                    self.keys[pygame.K_DOWN] = True
+                elif event.key == pygame.K_RIGHT:
+                    self.keys[pygame.K_RIGHT] = True
+                elif event.key == pygame.K_LEFT:
+                    self.keys[pygame.K_LEFT] = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    self.keys[pygame.K_UP] = False
+                elif event.key == pygame.K_DOWN:
+                    self.keys[pygame.K_DOWN] = False
+                elif event.key == pygame.K_RIGHT:
+                    self.keys[pygame.K_RIGHT] = False
+                elif event.key == pygame.K_LEFT:
+                    self.keys[pygame.K_LEFT] = False
+
+    def is_down_pressed(self):
+        return self.keys[pygame.K_DOWN]
+
+    def is_up_pressed(self):
+        return self.keys[pygame.K_UP]
+
+    def is_left_pressed(self):
+        return self.keys[pygame.K_LEFT]
+
+    def is_right_pressed(self):
+        return self.keys[pygame.K_RIGHT]
+
+    def __render_hud(self, current_step, steps):
+        text = self.font.render(
+            'Right arrow: next step', False, self.fontcolor)
+        rightarrow_rect = text.get_rect()
+        rightarrow_rect.left = self.padding
+        rightarrow_rect.top = self.height - rightarrow_rect.height - self.padding
+        self.screen.blit(text, rightarrow_rect)
+
+        text = self.font.render(
+            'Left arrow: previous step', False, self.fontcolor)
+        leftarrow_rect = text.get_rect()
+        leftarrow_rect.left = rightarrow_rect.left
+        leftarrow_rect.top = rightarrow_rect.top - leftarrow_rect.height
+        self.screen.blit(text, leftarrow_rect)
+
+        text = self.font.render(
+            'Up arrow: increase step', False, self.fontcolor)
+        uparrow_rect = text.get_rect()
+        uparrow_rect.left = self.width - uparrow_rect.width - self.padding
+        uparrow_rect.top = self.height - rightarrow_rect.height - self.padding
+        self.screen.blit(text, uparrow_rect)
+
+        text = self.font.render(
+            'Down arrow: decrease step', False, self.fontcolor)
+        downarrow_rect = text.get_rect()
+        downarrow_rect.left = self.width - downarrow_rect.width - self.padding
+        downarrow_rect.top = uparrow_rect.top - downarrow_rect.height
+        self.screen.blit(text, downarrow_rect)
+
+        text = self.font.render(
+            f'Current step: {current_step}', False, self.fontcolor)
+        self.screen.blit(text, (self.padding, 0))
+
+        text = self.font.render(f'Steps: {steps}', False, self.fontcolor)
+        step_rect = text.get_rect()
+        step_rect.left = self.width - step_rect.width - self.padding
+        step_rect.top = self.padding
+        self.screen.blit(text, step_rect)

0511d73d53e7355b785b005454ccde52c6e99095
Fillipe Rosini <filliperosini@gmail.com>
2018-12-23 23:29:45 -0200

    Finished day 14 part 2 in Python

--- python/day14.py
+++ python/day14.py
@@ -3,18 +3,33 @@ INPUT = 704321
 
 def part1(recipes):
     scores = '37'
-    elf1_index = 0
-    elf2_index = 1
+    score_gen = generate_scores(scores)
     for _ in range(recipes + 10):
-        elf1_score = int(scores[elf1_index])
-        elf2_score = int(scores[elf2_index])
-        result = str(elf1_score + elf2_score)
-        scores += result[0]
-        if len(result) > 1:
-            scores += result[1]
-        elf1_index = (elf1_index + elf1_score + 1) % len(scores)
-        elf2_index = (elf2_index + elf2_score + 1) % len(scores)
+        scores += next(score_gen)
     return scores[recipes:recipes + 10]
+
+
+def generate_scores(initial_scores):
+    scores = initial_scores
+    elf1 = 0
+    elf2 = 1
+    while True:
+        score1 = int(scores[elf1])
+        score2 = int(scores[elf2])
+        result = str(score1 + score2)
+        scores += result
+        yield result
+        elf1 = (elf1 + score1 + 1) % len(scores)
+        elf2 = (elf2 + score2 + 1) % len(scores)
+
+
+def part2(recipes):
+    scores = '37'
+    score_gen = generate_scores(scores)
+    length = len(recipes)
+    while recipes not in scores[-length:]:
+        scores += next(score_gen)
+    return scores.index(recipes)
 
 
 def main():
@@ -22,8 +37,13 @@ def main():
     assert part1(5) == '0124515891'
     assert part1(18) == '9251071085'
     assert part1(2018) == '5941429882'
-
     print(part1(INPUT))
+
+    assert part2('51589') == 9
+    assert part2('01245') == 5
+    assert part2('92510') == 18
+    assert part2('59414') == 2018
+    print(part2(str(INPUT)))
 
 
 if __name__ == "__main__":

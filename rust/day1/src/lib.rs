bfc8ea5d0650f330718fd005efecf564ac6ed1fb
Fillipe Rosini <hi@frosini.dev>
2019-11-25 23:40:55 -0300

    Finished day 1, part 2 in Rust

--- rust/day1/src/lib.rs
+++ rust/day1/src/lib.rs
@@ -1,21 +1,48 @@
 use std::num::ParseIntError;
 use std::fs;
 use std::error::Error;
+use std::collections::HashSet;
+
+pub enum Part {
+    Part1,
+    Part2,
+}
+
+impl Part {
+    pub fn new(raw_value: String) -> Result<Self, String> {
+        match raw_value.to_lowercase().as_str() {
+            "part1" => Ok(Part::Part1),
+            "part2" => Ok(Part::Part2),
+            _ => Err(format!("Invalid part: {}", raw_value)),
+        }
+    }
+}
 
 pub struct Config {
     pub filename: String,
+    pub part: Part,
 }
 
 impl Config {
-    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+    pub fn new(mut args: std::env::Args) -> Result<Config, String> {
         args.next();
+
+        let part = match args.next() {
+            Some(raw_part) => {
+                match Part::new(raw_part) {
+                    Ok(p) => p,
+                    Err(e) => return Err(e),
+                }
+            },
+            None => return Err(String::from("Didn't get a part")),
+        };
 
         let filename = match args.next() {
             Some(arg) => arg,
-            None => return Err("Didn't get a file name"),
+            None => return Err(String::from("Didn't get a file name")),
         };
 
-        Ok(Config { filename, })
+        Ok(Config { filename, part, })
     }
 }
 
@@ -23,9 +50,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
     let frequencies = parse_frequencies(&contents)?;
 
-    let final_frequency = calculate_frequency(&frequencies);
+    match config.part {
+        Part::Part1 => {
+            let final_frequency = calculate_frequency(&frequencies);
 
-    println!("{}", final_frequency);
+            println!("{}", final_frequency);
+        },
+        Part::Part2 => {
+            match calculate_repeating_frequency(&frequencies) {
+                Some(f) => println!("{}", f),
+                None => println!("Invalid frequency vector"),
+            }
+        }
+    }
 
     Ok(())
 }
@@ -36,6 +73,29 @@ pub fn parse_frequencies(contents: &str) -> Result<Vec<i32>, ParseIntError> {
 
 pub fn calculate_frequency(frequencies: &Vec<i32>) -> i32 {
     frequencies.iter().sum()
+}
+
+pub fn calculate_repeating_frequency(frequencies: &Vec<i32>) -> Option<i32> {
+    if frequencies.len() == 0 {
+        return None;
+    }
+
+    let frequency_iter = frequencies.iter().cycle();
+    let mut current_frequency = 0;
+
+    let mut frequency_set = HashSet::new();
+    frequency_set.insert(current_frequency);
+
+    for frequency in frequency_iter {
+        current_frequency += *frequency;
+        if frequency_set.contains(&current_frequency) {
+            break;
+        }
+
+        frequency_set.insert(current_frequency);
+    }
+
+    Some(current_frequency)
 }
 
 #[cfg(test)]
@@ -79,5 +139,35 @@ invalid";
     fn calculate_frequencies_third_example_should_return_minus_6() {
         let frequencies = vec![-1, -2, -3];
         assert_eq!(-6, calculate_frequency(&frequencies));
+    }
+
+    #[test]
+    fn calculate_repeating_frequency_empty_vec_should_return_none() {
+        let frequencies: Vec<i32> = Vec::new();
+        assert_eq!(None, calculate_repeating_frequency(&frequencies));
+    }
+
+    #[test]
+    fn calculate_repeating_frequency_first_example_should_return_0() {
+        let frequencies = vec![1, -1];
+        assert_eq!(Some(0), calculate_repeating_frequency(&frequencies));
+    }
+
+    #[test]
+    fn calculate_repeating_frequency_second_example_should_return_10() {
+        let frequencies = vec![3, 3, 4, -2, -4];
+        assert_eq!(Some(10), calculate_repeating_frequency(&frequencies));
+    }
+
+    #[test]
+    fn calculate_repeating_frequency_third_example_should_return_5() {
+        let frequencies = vec![-6, 3, 8, 5, -6];
+        assert_eq!(Some(5), calculate_repeating_frequency(&frequencies));
+    }
+
+    #[test]
+    fn calculate_repeating_frequency_fourth_example_should_return_14() {
+        let frequencies = vec![7, 7, -2, -7, -4];
+        assert_eq!(Some(14), calculate_repeating_frequency(&frequencies));
     }
 }
